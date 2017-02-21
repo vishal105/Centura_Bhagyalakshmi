97f5ba427f56f0ed335dce45ee86bee795b3ac1d
vishal <vishalsojitra.dh@gmail.com>
2017-02-21 17:28:48 +0530

    Vishal Login Api Done

--- app/src/main/java/materialtest/example/centura/centura_bhagyalakshmi/login/LoginActivity.java
+++ app/src/main/java/materialtest/example/centura/centura_bhagyalakshmi/login/LoginActivity.java
@@ -12,10 +12,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.RequestFuture;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 import materialtest.example.centura.centura_bhagyalakshmi.R;
 import materialtest.example.centura.centura_bhagyalakshmi.changepassword.ChangePasswordActivity;
@@ -30,6 +37,8 @@ import static materialtest.example.centura.centura_bhagyalakshmi.R.string.login;
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     Button bt_login;
     EditText et_username, et_password;
+    login loginmodel;
+    String URL ="http://192.168.0.144:81/api/BhagyaLakshmi/";
 
 
     public String username, password;
@@ -51,12 +60,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View v) {
+        loginmodel = new login(username,password);
         username=et_username.getText().toString().trim();
         password=et_password.getText().toString().trim();
         if(username.length()>=1) {
             if (password.length()>=1){
                 loginapi();
-                startActivity(new Intent(LoginActivity.this,DashBoardActivity.class));
+
 
 
         }else {
@@ -70,7 +80,32 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     private void loginapi() {
+        String LOGIN_URL=URL+"Login/"+"?UserName=" + username + "&&Password=" + password;
+        JsonObjectRequest jsonObjectRequest =new JsonObjectRequest(Request.Method.GET, LOGIN_URL
+                , new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(LoginActivity.this,DashBoardActivity.class));
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(LoginActivity.this,error.toString(),Toast.LENGTH_SHORT).show();
+
+            }
+
+    });
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(3000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+
+        RequestQueue requestQueue = Volley.newRequestQueue(LoginActivity.this);
+        requestQueue.add(jsonObjectRequest);
 
     }
 }
+
 
--- app/src/main/java/materialtest/example/centura/centura_bhagyalakshmi/model/login.java
+++ app/src/main/java/materialtest/example/centura/centura_bhagyalakshmi/model/login.java
@@ -5,5 +5,26 @@ package materialtest.example.centura.centura_bhagyalakshmi.model;
  */
 
 public class login {
+    private String UserName, Password;
 
+    public login(String userName, String password) {
+        UserName = userName;
+        Password = password;
+    }
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
 }
