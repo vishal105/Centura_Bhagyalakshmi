734dfa569507d127e61b6284c1592bdf96d93307
nammaBasava <basavaraju.kurubagowda@gmail.com>
2017-02-21 11:50:40 +0530

    change password updates

--- app/src/main/java/materialtest/example/centura/centura_bhagyalakshmi/changepassword/ChangePasswordActivity.java
+++ app/src/main/java/materialtest/example/centura/centura_bhagyalakshmi/changepassword/ChangePasswordActivity.java
@@ -14,7 +14,7 @@ public class ChangePasswordActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_change);
+        setContentView(R.layout.activity_changepassword);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
