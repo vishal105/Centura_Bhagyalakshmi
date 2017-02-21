b5264d9f9a1326ecb681a44166db14d9b8895e11
nammaBasava <basavaraju.kurubagowda@gmail.com>
2017-02-21 15:59:53 +0530

    important updates

--- app/src/main/java/materialtest/example/centura/centura_bhagyalakshmi/changepassword/ChangePasswordActivity.java
+++ app/src/main/java/materialtest/example/centura/centura_bhagyalakshmi/changepassword/ChangePasswordActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import materialtest.example.centura.centura_bhagyalakshmi.R;
@@ -21,6 +22,16 @@ public class ChangePasswordActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+        if(item.getItemId() == android.R.id.home ){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
