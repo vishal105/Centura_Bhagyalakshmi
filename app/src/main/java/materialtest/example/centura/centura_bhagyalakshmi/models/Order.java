ed30df906fdbed3b0185718599b9662ecd287303
Vanipavadi <vanipavadi5@gmail.com>
2017-02-24 15:16:05 +0530

    vani orderactivity

--- app/src/main/java/materialtest/example/centura/centura_bhagyalakshmi/models/Order.java
+++ app/src/main/java/materialtest/example/centura/centura_bhagyalakshmi/models/Order.java
@@ -3,57 +3,21 @@ package materialtest.example.centura.centura_bhagyalakshmi.models;
 import java.math.BigInteger;
 import java.util.ArrayList;
 
-/**
- * Created by Centura User1 on 07-12-2016.
- */
 
 public class Order {
     private String Id;
-    private ArrayList<OrderProduct> Products;
-    private OrderRole User;
-    private OrderRole Client;
-    private Double TotalAmount;
-    private String Status;
-    private String OrderNumber;
-    private String OrderDate;
+    private String Name;
     private BigInteger TimeStamp;
-    private String Company;
+    public static boolean LoadOrders = true;
 
+    public Order() {
+        Id = "";
+        Name = new String();
 
+        TimeStamp = BigInteger.valueOf(0);
 
-    public static boolean LoadOrders=true;
-
-    public Order(){
-        Id="";
-
-        Products=new ArrayList<OrderProduct>();
-        User=new OrderRole();
-        Client=new OrderRole();
-        TotalAmount=0.0;
-        Status="";
-        OrderNumber="";
-        TimeStamp= BigInteger.valueOf(0);
-        OrderDate="";
-        Company="";
     }
 
-
-
-    public String getCompany() {
-        return Company;
-    }
-
-    public void setCompany(String company) {
-        Company = company;
-    }
-
-    public static boolean isLoadOrders() {
-        return LoadOrders;
-    }
-
-    public static void setLoadOrders(boolean loadOrders) {
-        LoadOrders = loadOrders;
-    }
 
     public String getId() {
         return Id;
@@ -63,55 +27,15 @@ public class Order {
         Id = id;
     }
 
-    public ArrayList<OrderProduct> getProducts() {
-        return Products;
+
+    public String getName() {
+        return Name;
     }
 
-
-
-    public void setProducts(ArrayList<OrderProduct> products) {
-        Products = products;
+    public void setName(String name) {
+        Name = name;
     }
 
-    public OrderRole getUser() {
-        return User;
-    }
-
-    public void setUser(OrderRole user) {
-        User = user;
-    }
-
-    public OrderRole getClient() {
-        return Client;
-    }
-
-    public void setClient(OrderRole client) {
-        Client = client;
-    }
-
-    public Double getTotalAmount() {
-        return TotalAmount;
-    }
-
-    public void setTotalAmount(Double totalAmount) {
-        TotalAmount = totalAmount;
-    }
-
-    public String getStatus() {
-        return Status;
-    }
-
-    public void setStatus(String status) {
-        Status = status;
-    }
-
-    public String getOrderNumber() {
-        return OrderNumber;
-    }
-
-    public void setOrderNumber(String orderNumber) {
-        OrderNumber = orderNumber;
-    }
 
     public BigInteger getTimeStamp() {
         return TimeStamp;
@@ -121,11 +45,5 @@ public class Order {
         TimeStamp = timeStamp;
     }
 
-    public String getOrderDate() {
-        return OrderDate;
-    }
 
-    public void setOrderDate(String orderDate) {
-        OrderDate = orderDate;
-    }
 }
