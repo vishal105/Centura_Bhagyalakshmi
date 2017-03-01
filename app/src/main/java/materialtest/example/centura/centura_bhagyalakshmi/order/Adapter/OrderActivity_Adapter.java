e350fa02be24a43fe3a840923b6f309899f628c9
VISHAL <vishalsojitra.dh@gmail.com>
2017-03-01 11:24:51 +0530

    vishal recyclerview done with api integration

--- app/src/main/java/materialtest/example/centura/centura_bhagyalakshmi/order/Adapter/OrderActivity_Adapter.java
+++ app/src/main/java/materialtest/example/centura/centura_bhagyalakshmi/order/Adapter/OrderActivity_Adapter.java
@@ -21,7 +21,7 @@ public class OrderActivity_Adapter  extends RecyclerView.Adapter<OrderActivity_A
 
     private static String LOG_TAG ="OrderActivity_Adapter";
     public static ArrayList<Order> mdataset;
-    private static MyClickListener myClickListener;
+   /* private static MyClickListener myClickListener;*/
     Context mcontext;
     ArrayList<Order> data;
 
@@ -40,44 +40,8 @@ public class OrderActivity_Adapter  extends RecyclerView.Adapter<OrderActivity_A
             tv_order_id= (TextView) itemView.findViewById(R.id.tv_order_id);
             tv_order_name= (TextView) itemView.findViewById(R.id.tv_order_name);
             tv_order_status= (TextView) itemView.findViewById(R.id.tv_order_status);
-
         }
-
-
-    }
-
-    public void setOnItemClickListener(MyClickListener myClickListener) {
-        OrderActivity_Adapter.myClickListener = myClickListener;
-    }
-
-
-
-
-    /* public class ViewHolder extends RecyclerView.ViewHolder{
-        public TextView tv_order_id,tv_order_name,tv_order_status;
-
-        public ViewHolder(View itemView) {
-            super(itemView);
-            tv_order_id= (TextView) itemView.findViewById(R.id.tv_order_id);
-            tv_order_name= (TextView) itemView.findViewById(R.id.tv_order_name);
-            tv_order_status= (TextView) itemView.findViewById(R.id.tv_order_status);
         }
-    }*/
-   /* public void add(int position, String item){
-        mdataset.add(position,item);
-        notifyItemInserted(position);
-    }
-    public void remove(String item){
-
-        int position = mdataset.indexOf(item);
-        mdataset.remove(position);
-        notifyItemRemoved(position);
-    }
-*/
-   /* public OrderActivity_Adapter(ArrayList<OrderObject> mydataset){
-        mdataset = mydataset;
-    }*/
-
     @Override
     public OrderActivity_Adapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.order_item,parent,false);
@@ -87,34 +51,15 @@ public class OrderActivity_Adapter  extends RecyclerView.Adapter<OrderActivity_A
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        holder.tv_order_id.setText(mdataset.get(position).getId());
-        holder.tv_order_name.setText(mdataset.get(position).getOrderNumber());
+        holder.tv_order_id.setText(mdataset.get(position).getOrderNumber());
+        holder.tv_order_name.setText(mdataset.get(position).getClient().getName());
         holder.tv_order_status.setText(mdataset.get(position).getStatus());
     }
-
-    public void addItem(Order dataObj, int index) {
-        mdataset.add(dataObj);
-        notifyItemInserted(index);
-    }
-
-    public void deleteItem(int index) {
-        mdataset.remove(index);
-        notifyItemRemoved(index);
-    }
-
 
     @Override
     public int getItemCount() {
         return mdataset.size();
     }
-
-    public interface MyClickListener{
-
-        void onItemClick(int position, View v);
-
-
-
     }
-}
 
 
