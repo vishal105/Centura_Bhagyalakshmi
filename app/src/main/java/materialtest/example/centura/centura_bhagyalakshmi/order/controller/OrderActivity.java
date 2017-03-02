92b2cb89fd1db74c4723bc9b80db1f15836ab21b
Vanipavadi <vanipavadi5@gmail.com>
2017-03-02 15:14:12 +0530

    intent done

--- app/src/main/java/materialtest/example/centura/centura_bhagyalakshmi/order/controller/OrderActivity.java
+++ app/src/main/java/materialtest/example/centura/centura_bhagyalakshmi/order/controller/OrderActivity.java
@@ -2,6 +2,7 @@ package materialtest.example.centura.centura_bhagyalakshmi.order.controller;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -17,6 +18,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
 
@@ -35,6 +41,11 @@ public class OrderActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager mLayoutManager;
     private static ArrayList<Order> orderList = new ArrayList<Order>();
     FloatingActionButton fb_addorder;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,18 +58,21 @@ public class OrderActivity extends AppCompatActivity {
         fb_addorder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                All_api.Orderapi(OrderActivity.this);
-                startActivity(new Intent(OrderActivity.this, DistrubutorSearchActivity.class));
-            }
+                //All_api.Orderapi(OrderActivity.this);
+                // startActivity(new Intent(OrderActivity.this, DistrubutorSearchActivity.class));
+                        startActivity(new Intent(OrderActivity.this,DistrubutorSearchActivity.class));
+                    }
         });
 
         orderRecyclerView = (RecyclerView) findViewById(R.id.rv_order);
-        orderRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this, orderRecyclerView ,new RecyclerItemClickListener.OnItemClickListener() {
-                    @Override public void onItemClick(View view, int position) {
+        orderRecyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this, orderRecyclerView, new RecyclerItemClickListener.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(View view, int position) {
                         startActivity(new Intent(OrderActivity.this, Select_order_activity.class));
                     }
 
-                    @Override public void onLongItemClick(View view, int position) {
+                    @Override
+                    public void onLongItemClick(View view, int position) {
                         Toast.makeText(OrderActivity.this, "Click once that is more than enough", Toast.LENGTH_SHORT).show();
                     }
                 })
@@ -70,6 +84,9 @@ public class OrderActivity extends AppCompatActivity {
         mAdapter = new OrderActivity_Adapter(this);
         InitializeAdapter(OrderActivity.this);
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void InitializeAdapter(Context context) {
@@ -78,7 +95,6 @@ public class OrderActivity extends AppCompatActivity {
         RecyclerView.ItemDecoration itemDecoration = new DividerItemDecoration(this, LinearLayoutManager.HORIZONTAL);
         orderRecyclerView.addItemDecoration(itemDecoration);
     }
-
 
 
     @Override
@@ -94,6 +110,42 @@ public class OrderActivity extends AppCompatActivity {
     public void onBackPressed() {
         super.onBackPressed();
         finish();
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Order Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 
 
